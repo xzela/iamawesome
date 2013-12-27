aa844a5603cbf207865ce7d25f57033efbcb97e2
Zeph Lafassett <zeph@lafassett.com>
2013-12-27 15:03:18 -0800

    add handlebars and backbone client side functions

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,3 +7,29 @@ var BookCollection = Backbone.Collection.extend({
 	url: "/books"
 });
 
+
+var BookView = Backbone.View.extend({
+	tagName: "li",
+	className: "book",
+	render: function() {
+		var template = $("#booktemplate").html();
+		var compiled = Handlebars.compile(template);
+		var html = compiled(this.model.attributes);
+
+		this.$el.html(html);
+
+		return this;
+	}
+});
+
+var BookCollectionView = Backbone.View.extend({
+	tagName: "ul",
+	className: "books",
+	render: function() {
+		this.collection.each(function(book) {
+			var bookView = new BookView({model: book});
+			this.$el.append(bookView.render().el);
+		}, this);
+		return this;
+	}
+});
--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,3 +1,10 @@
 $(function() {
 	// some init code here
+	var collection = new BookCollection();
+	collection.fetch({
+		success: function(data) {
+			var view = new BookCollectionView({collection: data});
+			$("body").append(view.render().el);
+		}
+	});
 });
